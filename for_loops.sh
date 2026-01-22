2013778fb04fba1775905ad79ed27b55f98a0788
Ubuntu <ubuntu@ip-172-31-19-26.eu-west-1.compute.internal>
2026-01-22 06:03:57 +0000

    added shell scripts

---
+++ for_loops.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+villan is virus
---
+++ hello_chatur.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+
+echo "Chatur: Adarniya Sabhapati mahodya"
+
+echo "chatur: aaj hum sab milkar chamatkar karenge"
+
+mkdir -p rancho
+
+echo "rancho: ye leh,ban diya, ro mat ab"
+
+echo "abba nahi manenge" > farhanitrate.txt
