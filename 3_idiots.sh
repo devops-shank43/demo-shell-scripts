a91b6f6d86518927f509dd8bdab639584741186c
Ubuntu <ubuntu@ip-172-31-19-26.eu-west-1.compute.internal>
2026-01-22 05:51:31 +0000

    added 3_idiots.sh

---
+++ 3_idiots.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+#user defined variables
+hero="rancho"
+villain="virus"
+
+
+echo "3 idiots ka hero hai $hero"
+
+echo "3 idiots ka villain hai $villain"
+
+# shell / enviornment variables bhi hote hai
+
+echo "current logged in user $USER"
